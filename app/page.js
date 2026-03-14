85595696edfad225af929b9dde5a8fb6f45ccf2a
jmelnickmacdonald <julianna@indiciumx.com>
2026-03-13 22:42:43 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+'use client';
+
+import CloverPosts from '@/components/CloverPosts';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-b from-green-50 to-white">
+      <CloverPosts />
+    </main>
+  );
+}
